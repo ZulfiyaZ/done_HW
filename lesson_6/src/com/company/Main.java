022669eb00ccf0a87b7769fbf78c8c2514474165
ZulfiyaZ <zulfiyag26@gmail.com>
2022-02-17 13:51:06 +0600

    Add new HW1

--- lesson_6/src/com/company/Main.java
+++ lesson_6/src/com/company/Main.java
@@ -3,25 +3,21 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int a = 0;
-        int b = 100;
+        int a = 10;
+        int b = 10;
+        int c = 100;
+        int d = 49;
 
-        int random_number1 = a + (int) (Math.random() * b);
-        int random_number2 = a + (int) (Math.random() * b);
-        int random_number3 = a + (int) (Math.random() * b);
-        int random_number4 = a + (int) (Math.random() * b);
-
-        int sum_1 = random_number1 + random_number2;
-        int sum_2 = random_number3 + random_number4;
+        int sum_1 = a + b;
+        int sum_2 = c + d;
         boolean result_sum = sum_1 > sum_2;
         System.out.println(result_sum);
 
-        int sum_tot_1 = sum_1 + 1;
-        int sum_tot_2 = sum_2 - 2;
-        boolean sum_tot = (sum_tot_1 > sum_tot_2);
+        sum_1 += 1;
+        sum_2 -= 2;
+        boolean sum_tot = (sum_1 > sum_2);
         System.out.println(sum_tot);
 
-        boolean res_tot = (sum_tot_1 % 2 != 0) || (sum_tot_2% 2 != 0);
-        System.out.println(res_tot);
+        System.out.println((sum_1 % 2 == 0 & sum_2 % 2 == 0) || (sum_1 % 2 == 0) || (sum_2 % 2 == 0));
     }
 }
